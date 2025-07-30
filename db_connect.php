c461616a9929e1e670b1ea5c543f29a754419167
geetha-medha <geethanjali@medhatech.in>
2025-07-30 11:26:23 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,18 +1,18 @@
 <?php
-// $host = '192.185.129.71';
-// $db = 'medha_mira';
-// $user = 'medha_mira_admin'; 
-// $pass = 'T^vF]OsVtc4a';
-// $conn = new mysqli($host, $user, $pass, $db);
-// if ($conn->connect_error) {
-//     die('Database connection failed: ' . $conn->connect_error);
-// }
-
-$host = 'localhost';
-$db = 'mira_chatbot';
-$user = 'root'; 
-$pass = '';
+$host = '192.185.129.71';
+$db = 'medha_mira';
+$user = 'medha_mira_admin'; 
+$pass = 'T^vF]OsVtc4a';
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die('Database connection failed: ' . $conn->connect_error);
 }
+
+//$host = 'localhost';
+//$db = 'mira_chatbot';
+//$user = 'root'; 
+//$pass = '';
+//$conn = new mysqli($host, $user, $pass, $db);
+//if ($conn->connect_error) {
+//    die('Database connection failed: ' . $conn->connect_error);
+//}
